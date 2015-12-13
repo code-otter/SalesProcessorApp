8e6dd4a330df486b9fa93534e6738c969f88df4e
Swinside <swinside@localhost.localdomain>
2015-12-13 23:43:58 +0100

    Ajout des generated au gitignore

--- src/main/generated/fr/codeotter/salesservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://codeotter.fr/salesservice.wsdl", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.codeotter.salesservice;
